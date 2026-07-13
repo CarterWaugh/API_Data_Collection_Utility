89ca7a64bfeedb6f5ef3d70b62cd5ab1737ec319
Carter Waugh <carterw@youngsoft.com>
2026-07-13 12:27:15 -0400

    updated how errors are handled

--- API_Data_Collection.py
+++ API_Data_Collection.py
@@ -11,13 +11,10 @@ url = f"https://wttr.in/{city}?format=j1"
 # Starts try block
 try:
     response = requests.get(url)
-    print(f"Server Status Code: {response.status_code}")
 
-# Checks status code
+    # SUCCESS: Checks if status code is 200 (OK)
     if response.status_code == 200:
-
         data = response.json()
-
         current_weather = data['current_condition'][0]
 
         temp = current_weather['temp_F']
@@ -29,12 +26,21 @@ try:
 
         print()
         print(f"In the city of {city.capitalize()}...")
-        print(f"The temperature(F) is : {temp}°, but feels like: {feel_temp}°.")
-        print(f"The humidity level is a: {humid}.")
+        print(f"The temperature (F) is: {temp}°, but feels like: {feel_temp}°.")
+        print(f"The humidity level is: {humid}%.")
         print(f"The uv index is at a: {uv}.")
         print(f"The direction of the wind on a 16 point is: {wind_dir}, and the speed is {wind_speed} MPH.")
         print()
 
-# Reveals what error occured
+    # KNOWN ERRORS: returns what happened if city isnt found
+    elif response.status_code in [404, 500]:
+        print(f"\nServer Status Code: {response.status_code}")
+        print("City not found. Please check your spelling and try again.\n")
+
+    # UNKNOWN ERRORS: Any other status code
+    else:
+        print(f"\nFailed to retrieve data. Server returned status: {response.status_code}\n")
+
+# Reveals what unexpected error occurred
 except Exception as e:
-    print(f"An error occurred: {e}")
+    print(f"\nThere was an error: {e}\n")
